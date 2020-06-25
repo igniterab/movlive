402d1e23401526aecda9d12e227b71625b0bb1b6
igniterab <ashishbhardwajexcel@gmail.com>
2020-06-25 12:30:40 +0530

    changed allowed hosts

--- movie/settings.py
+++ movie/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z&m!!e833n6u4r$z$+ziyj96)+7jf242lgp56u!&4!c)*ie#lm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['movie404.herokuapp.com']
 
 
 # Application definition
